0a613d3187806a27c54bf2ce9f06e6d5e6d84c7d
Psycholab.ljz <psyljz@gmail.com>
2024-07-24 20:07:35 +0800

    add nftsign and pay with eth

--- src/NftMarket.sol
+++ src/NftMarket.sol
@@ -1,126 +1,200 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-/// @title NFT Market
-/// @author liujingze
-/// @dev This contract is a simple NFT market contract
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract NftMarket {
-
-
     error NftMarketplace_NotApprovedForMarketplace();
-    error NftMarketplace_NotListed(address nftAddress, uint256 tokenId);
+    error NftMarketplace_NotListed(address nftAddress, uint256 tokenId, address payToken);
     error NftMarketplace_NotEnoughFunds();
+    error NftMarketplace_InvalidPayment();
+    
     event NftMarketplace_Listed(
         address indexed token_address,
         uint256 indexed tokenId,
-        uint256 sale_price
+        uint256 sale_price,
+        address payToken
     );
     event NftMarketplace_Bought(
         address indexed token_address,
         uint256 indexed tokenId,
-        address indexed buyer
+        address indexed buyer,
+        uint256 price,
+        address payToken
     );
-    // The address of the ERC20 token used for trading
+
     address public immutable IEC20_TOKEN_ADDRESS;
     address public immutable NFT_OWNER;
+
     struct EIP712Domain {
         string name;
         string version;
         uint256 chainId;
         address verifyingContract;
     }
-    struct Nft_witheList {
+    struct NftWhiteList {
         address wallet;
     }
-    bytes32 constant EIP712DOMAIN_TYPEHASH = keccak256(
-        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-    );
 
-    bytes32 constant Nft_witheList_TYPEHASH = keccak256(
-        "Nft_witheList(address wallet)"
-    );
+    struct LimitOrder {
+        address seller;
+        address nft;
+        uint256 tokenId;
+        address payToken;
+        uint256 price;
+        uint256 deadline;
+    }
+    
+    bytes32 constant EIP712DOMAIN_TYPEHASH =
+        keccak256(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        );
 
-    // The list of NFTs on the market
-    mapping(address => mapping(uint256 => uint256)) public nftList;
+    bytes32 constant NFT_WHITHE_TYPEHASH =
+        keccak256("Nft_witheList(address wallet)");
 
+    bytes32 private constant LIMIT_ORDER_TYPE_HASH =
+        keccak256(
+            "LimitOrder(address seller,address nft,uint256 tokenId,address payToken,uint256 price,uint256 deadline)"
+        );
+
+    // 为了支持多种代币支付
+    mapping(address => mapping(uint256 => mapping(address => uint256))) public nftList;
 
     constructor(address _IEC20_TOKEN, address _NFT_OWNER) {
         IEC20_TOKEN_ADDRESS = _IEC20_TOKEN;
-        NFT_OWNER =_NFT_OWNER ;
-       
+        NFT_OWNER = _NFT_OWNER;
     }
 
     function DOMAIN_SEPARATOR() public view returns (bytes32) {
         return
             keccak256(
                 abi.encode(
-                    // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
-                    0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
+                    EIP712DOMAIN_TYPEHASH,
                     keccak256("NftMarket"),
                     keccak256("1"),
-                    "1",
+                    block.chainid,
                     address(this)
                 )
             );
     }
 
-    function listItem(
-        address token_address,
-        uint256 tokenId,
-        uint sale_price
-    ) public {
+    function listItem(address token_address, uint256 tokenId, uint256 sale_price, address payToken) public {
         IERC721 nft = IERC721(token_address);
-        if (nft.getApproved(tokenId) != address(this)) {
-            revert NftMarketplace_NotApprovedForMarketplace();
-        }
-        nftList[token_address][tokenId] = sale_price;
-        emit NftMarketplace_Listed(token_address, tokenId, sale_price);
+        require(nft.ownerOf(tokenId) == msg.sender, "Not the owner");
+        require(nft.getApproved(tokenId) == address(this), "Not approved for marketplace");
+        
+        nftList[token_address][tokenId][payToken] = sale_price;
+        emit NftMarketplace_Listed(token_address, tokenId, sale_price, payToken);
     }
-    
+
+    function buy_with_signatures(
+        LimitOrder memory limitOrder,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public payable {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR(),
+                keccak256(
+                    abi.encode(
+                        LIMIT_ORDER_TYPE_HASH,
+                        limitOrder.seller,
+                        limitOrder.nft,
+                        limitOrder.tokenId,
+                        limitOrder.payToken,
+                        limitOrder.price,
+                        limitOrder.deadline
+                    )
+                )
+            )
+        );
+        require(
+            ecrecover(digest, v, r, s) == limitOrder.seller,
+            "invalid signature"
+        );
+        require(block.timestamp <= limitOrder.deadline, "Order expired");
+        _buynft(limitOrder.nft, limitOrder.tokenId, limitOrder.price, limitOrder.payToken);
+    }
+
     function _buynft(
         address token_address,
         uint256 tokenId,
-        uint buy_token_amount
+        uint256 buy_token_amount,
+        address payToken
     ) internal {
-        if (nftList[token_address][tokenId] <= 0) {
-            revert NftMarketplace_NotListed(token_address, tokenId);
+        uint256 price = nftList[token_address][tokenId][payToken];
+        if (price <= 0) {
+            revert NftMarketplace_NotListed(token_address, tokenId, payToken);
         }
 
-        if (buy_token_amount < nftList[token_address][tokenId]) {
+        if (buy_token_amount < price) {
             revert NftMarketplace_NotEnoughFunds();
         }
 
-        IERC20(IEC20_TOKEN_ADDRESS).transferFrom(
-            msg.sender,
-            address(this),
-            nftList[token_address][tokenId]
-        );
         IERC721 nft = IERC721(token_address);
-        nft.safeTransferFrom(nft.ownerOf(tokenId), msg.sender, tokenId);
-        emit NftMarketplace_Bought(token_address, tokenId, msg.sender);
+        address seller = nft.ownerOf(tokenId);
+
+        if (payToken == address(0)) {
+            // ETH payment
+            if (msg.value < price) {
+                revert NftMarketplace_NotEnoughFunds();
+            }
+            payable(seller).transfer(price);
+            if (msg.value > price) {
+                payable(msg.sender).transfer(msg.value - price);
+            }
+        } else {
+            // ERC20 payment
+            if (msg.value > 0) {
+                revert NftMarketplace_InvalidPayment();
+            }
+            IERC20(payToken).transferFrom(msg.sender, seller, price);
+        }
+
+        nft.safeTransferFrom(seller, msg.sender, tokenId);
+        delete nftList[token_address][tokenId][payToken];
+
+        emit NftMarketplace_Bought(token_address, tokenId, msg.sender, price, payToken);
     }
-    function hashStruct(EIP712Domain memory eip712Domain) internal pure returns (bytes32) {
-        return keccak256(
-        abi.encode(
-        EIP712DOMAIN_TYPEHASH,
-        keccak256(bytes(eip712Domain.name)),
-        keccak256(bytes(eip712Domain.version)),
-        eip712Domain.chainId,
-        eip712Domain.verifyingContract
-)
-);
+
+    function hashStruct(
+        EIP712Domain memory eip712Domain
+    ) internal pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    EIP712DOMAIN_TYPEHASH,
+                    keccak256(bytes(eip712Domain.name)),
+                    keccak256(bytes(eip712Domain.version)),
+                    eip712Domain.chainId,
+                    eip712Domain.verifyingContract
+                )
+            );
     }
-    
-    function verifyBuy(address wallet, uint8 v, bytes32 r, bytes32 s,address token_address,uint256 tokenId,uint256 buy_token_amount) public {
-   
-    bytes32 digest = keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR(), keccak256(abi.encode(Nft_witheList_TYPEHASH, wallet))));
-    require(ecrecover(digest, v, r, s)==NFT_OWNER, "not whitelisted");
 
-    _buynft(token_address, tokenId, buy_token_amount);
+    function verifyBuy(
+        address wallet,
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        address token_address,
+        uint256 tokenId,
+        uint256 buy_token_amount,
+        address payToken
+    ) public payable {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR(),
+                keccak256(abi.encode(NFT_WHITHE_TYPEHASH, wallet))
+            )
+        );
+        require(ecrecover(digest, v, r, s) == NFT_OWNER, "not whitelisted");
 
-}
-
+        _buynft(token_address, tokenId, buy_token_amount, payToken);
+    }
 }
